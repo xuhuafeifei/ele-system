df5e2feb47a562bac0b9f44ea1c2524443c95730
xuhuafeifei <2508020102@qq.com>
2025-05-09 20:38:44 +0800

    fix(core): 修改调度器引用

--- src/main/java/com/fgbg/ele/elevator/Connection.java
+++ src/main/java/com/fgbg/ele/elevator/Connection.java
@@ -3,7 +3,7 @@ package com.fgbg.ele.elevator;
 /**
  * 连接通道. 模拟redis客户端向redis发送请求.
  * 在电梯调度系统中, 则扮演的是{@link FloorClient}向{@link Elevator}发送楼层请求{@link FloorRequest}
- * 每一个请求都会被放入{@link RequestPool}中, 然后由{@link Scheduler}调度器进行调度.
+ * 每一个请求都会被放入{@link RequestPool}中, 然后由{@link com.fgbg.ele.schedule.MyScheduledTasks}调度器进行调度.
  * @author feigebuge
  * @email 2508020102@qq.com
  */
